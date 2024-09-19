90a3c236a5967ed565f510839491c191c5c703b5
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2024-09-18 17:53:10 -0700

    removing duplicate query providers :fire:

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import { Provider } from "@/components/Providers";
-import ReactQueryProvider from "@/components/ReactQueryProvider";
 
 const lexend = Lexend({ subsets: ["latin"] });
 
@@ -20,12 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn(lexend.className, "antialiased min-h-screen pt-16")}>
-        <ReactQueryProvider>
           <Provider>
             <Navbar />
             {children}
           </Provider>
-        </ReactQueryProvider>
       </body>
     </html>
   );
